ab5f4c6d40b1a6a2caba117b9c8097678e52375b
tanaya2005 <tanayaitis@gmail.com>
2025-02-14 12:52:12 +0530

    .

--- kindmeals/BACKEND/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://tanaya:mongotan@cluster0.9e5vj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=> console.log("Database connected"));
-    
-}
